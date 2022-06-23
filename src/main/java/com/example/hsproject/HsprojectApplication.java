77c3925f5de4d429419e307641c5971cb768483e
USER <softj0106@gmail.com>
2022-06-23 22:11:14 +0900

    Spring boot Example

---
+++ src/main/java/com/example/hsproject/HsprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.hsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HsprojectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/hsproject/controller/HsController.java
@@ -0,0 +1,16 @@
+package com.example.hsproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HsController {
+
+    @GetMapping("/hi")
+    public String Hi(Model model){
+        model.addAttribute("username","JHs");
+        return "greetings";// templates/greetings.mustache -> 브라우저로 전송!
+
+    }
+}
