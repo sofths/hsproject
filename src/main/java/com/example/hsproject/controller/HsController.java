f440ea37da336c43a6b355740618b6100b2cf4e2
USER <softj0106@gmail.com>
2022-06-27 00:04:42 +0900

    Changes

--- src/main/java/com/example/hsproject/controller/HsController.java
+++ src/main/java/com/example/hsproject/controller/HsController.java
@@ -6,11 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HsController {
-
     @GetMapping("/hi")
     public String Hi(Model model){
         model.addAttribute("username","JHs");
         return "greetings";// templates/greetings.mustache -> 브라우저로 전송!
-
+    }
+    @GetMapping("/bye")
+    public String Bye(Model model){
+        model.addAttribute("name","JHs");
+        return "goodbye";
     }
 }
